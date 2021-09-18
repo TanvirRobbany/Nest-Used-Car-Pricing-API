caa6f6e65a59a067e6109838ae24099676581afa
TanvirRobbany <trobbany@cisscom.com>
2021-09-18 22:11:53 +0600

    Connected Report Entity with the App

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-import {User} from './users/user.entity';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'sqlite',
     database: 'db.sqlite',
-    entities: [User],
+    entities: [User, Report],
     synchronize: true
   }),
-  UsersModule, ReportsModule],
+    UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
