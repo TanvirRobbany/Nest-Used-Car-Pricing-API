723a56e7819a62d462410ae3503672e5f4e6bfe1
TanvirRobbany <trobbany@cisscom.com>
2021-09-21 23:44:56 +0600

    Modified Report Controller and Service to Handle Creating a Report and Save in the Database

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { Report } from './schemas/report.schema';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+    constructor(@InjectModel('Reports') private repo: Model<Report>) { }
+
+    async create(reportDto: CreateReportDto) {
+        const newReport = new this.repo(reportDto);
+
+        const report = await newReport.save()
+        return report;
+    }
+}
--- src/reports/schemas/report.schema.ts
+++ src/reports/schemas/report.schema.ts
@@ -1,22 +1,22 @@
 import * as mongosse from 'mongoose';
 
 export const ReportSchema = new mongosse.Schema({
-    price: String,
     make: String,
     model: String,
+    mileage: Number,
     year: Number,
     longitude: Number, 
     latitude: Number,
-    mileage: Number
+    price: Number
 })
 
 export interface Report {
     _id: string;
-    price: string;
     make: string;
     model: string;
+    mileage: number;
     year: number;
     longitude: number;
     latitude: number;
-    mileage: number;
+    price: number;
 }
